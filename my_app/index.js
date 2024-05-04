e688e90702bfb274bb2c57f1c549219c99d9e8cc
discphy <dinophy@nate.com>
2024-05-04 14:02:24 +0900

    feat(add): index template

--- my_app/index.js
+++ my_app/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const app = express();
 
 // middleware
+app.get("/", (req, res) => {
+    // res.send("Main page GET request");
+    // res.send("<h1>Hello main page</h1>");
+    res.sendFile(__dirname + "/public/main.html");
+})
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running");
+});
