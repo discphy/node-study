62203d2e02e8fde433c11ad0ab4435a9f8ff025d
discphy <dinophy@nate.com>
2024-05-06 17:11:42 +0900

    feat(add): nunjucks

--- my_app/index.js
+++ my_app/index.js
@@ -1,15 +1,29 @@
 import express from 'express';
 import path from 'path';
+import nunjucks from 'nunjucks';
 
 const __dirname = path.resolve();
 
 const app = express();
+
+// view engine set
+app.set("view engine", "html");
+
+// nunjucks
+nunjucks.configure('views', {
+    watch: true,
+    express: app
+})
 
 // middleware
 app.get("/", (req, res) => {
     // res.send("Main page GET request");
     // res.send("<h1>Hello main page</h1>");
     res.sendFile(__dirname + "/public/main.html");
+})
+
+app.get("/write", (req, res) => {
+    res.render("write");
 })
 
 app.listen(3000, () => {
